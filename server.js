5010f1f0416bf74ee3ad20f34c6f65525c57f75a
Divyansh Kumar <divyanshkumar528@gmail.com>
2024-03-29 23:41:52 +0530

    till here the database connection has been successfully done and some other task like how morgan is utilized for the debugging understood

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 import express from 'express';
 import colors from 'colors';
 import dotenv from 'dotenv';
+import morgan from 'morgan';
+import connectDB from './config/db.js';
 
 // configure env
 dotenv.config();
 
+// database config
+connectDB();
+
 // rest object
 const app = express();
+
+// middleware
+app.use(morgan('dev'));
+app.use(express.json());
 
 // rest api
 app.get("/", (req,res) => {
