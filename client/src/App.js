c63c771c60bb5cad03ba8f8546a38d90b1bb0a8c
Divyansh Kumar <divyanshkumar528@gmail.com>
2024-04-04 15:54:54 +0530

    till now the front page and the folder to contain all the component has been created

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+import './App.css';
+import Layout from './components/Layout/Layout';
+
+function App() {
+  return (
+    <>
+      <Layout>
+      <h1>Ecommerce App</h1>
+      </Layout>
+      
+    </>
+  );
+}
+
+export default App;
