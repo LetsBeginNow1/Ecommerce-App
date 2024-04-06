7a41c6ce85862ab09a0b8fb4250e5b0b14f71fdd
Divyansh Kumar <divyanshkumar528@gmail.com>
2024-04-07 00:10:59 +0530

    till here the basic styling of all the conponent in footer has been implemented and the routes which are not created will be directed to pagenot found from here on we will be working on search engine optimization because react by default not support this

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -4,7 +4,20 @@ import Layout from '../components/Layout/Layout'
 const About = () => {
   return (
     <Layout>
-        <h1>About Us</h1>
+        <div className="row contactus ">
+        <div className="col-md-6 ">
+          <img
+            src="/images/about.jpeg"
+            alt="contactus"
+            style={{ width: "50%" }}
+          />
+        </div>
+        <div className="col-md-4">
+          <p className="text-justify mt-2">
+          Welcome to Paudha Mandi, your premier destination for all things green. As a dedicated platform for plant enthusiasts, we are more than just a marketplace—we are a thriving ecosystem where the beauty of nature meets the convenience of online shopping.
+          </p>
+        </div>
+      </div>
     </Layout>
   );
 };
