d99adf1b24bd6dbbbc178bc2c8edcc999eaab1ab
Divyansh Kumar <divyanshkumar528@gmail.com>
2024-04-09 19:41:10 +0530

    till here the page refreshing action upon submission feature have been disabled using an event handler and the credentials will be shown to console for development point of view

--- client/src/components/Layout/Footer.js
+++ client/src/components/Layout/Footer.js
@@ -6,11 +6,11 @@ const Footer = () => {
     <div className='footer'>
         <h4 className='text-center'>All Right Reserved &copy; Paudha Mandi</h4>
         <p className='text-center mt-3'>
-          <Link to="/about">About</Link>
+          <Link to="/about"> About </Link>
           |
-          <Link to="/contact">Contact Us</Link>
+          <Link to="/contact"> Contact Us </Link>
           |
-          <Link to="/policy">Privacy Policy</Link>
+          <Link to="/policy"> Privacy Policy</Link>
           
         </p>
     </div>
--- client/src/pages/Auth/Register.js
+++ client/src/pages/Auth/Register.js
@@ -1,26 +1,74 @@
-import React from 'react'
-import Layout from '../../components/Layout/Layout'
+import React,{ useState } from 'react';
+import Layout from '../../components/Layout/Layout';
 
 const Register = () => {
+    const [name,setName] = useState("");
+    const [email,setEmail] =useState("");
+    const [password,setPassword] =useState("");
+    const [phone,setPhone] =useState("");
+    const [address,setAddress] =useState("");
+
+// form function
+const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name, email, password, phone, address);
+};
+    
   return (
     <Layout title={"Register - Ecommerce App"}>
         <div className='register'>
             <h1>Register Page</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="mb-3">
-                    <input type="text" className="form-control" id="exampleInputEmail1" placeholder="Enter Your Name" />
+                    <input type="text" 
+                        value={name} 
+                        onChange={(e) => setName(e.target.value)}
+                        className="form-control" 
+                        id="exampleInputEmail1" 
+                        placeholder="Enter Your Name" 
+                        required
+                    />
                 </div>
                 <div className="mb-3">
-                    <input type="email" className="form-control" id="exampleInputEmail1"  placeholder="Enter Your Email" />
+                    <input 
+                        type="email" 
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        className="form-control"
+                        id="exampleInputEmail1"  
+                        placeholder="Enter Your Email" 
+                        required
+                    />
                 </div>
                 <div className="mb-3">
-                    <input type="password" className="form-control" id="exampleInputPassword1" 
-                    placeholder="Enter Your Password"/>
+                    <input type="password" 
+                        value={password} 
+                        onChange={(e) => setPassword(e.target.value)}
+                        className="form-control" 
+                        id="exampleInputPassword1" 
+                        placeholder="Enter Your Password"
+                        required
+                    />
                 </div>
                 <div className="mb-3">
-                    <input type="text" className="form-control" id="exampleInputEmail1" placeholder="Enter Your Phone" />
+                    <input type="text" 
+                        value={phone} 
+                        onChange={(e) => setPhone(e.target.value)}
+                        className="form-control" 
+                        id="exampleInputEmail1" 
+                        placeholder="Enter Your Phone" 
+                        required
+                    />
                 </div><div className="mb-3">
-                    <input type="text" className="form-control" id="exampleInputEmail1" placeholder="Enter Your Address" />
+                    <input 
+                        type="text" 
+                        value={address} 
+                        onChange={(e) => setAddress(e.target.value)}
+                        className="form-control" 
+                        id="exampleInputEmail1" 
+                        placeholder="Enter Your Address" 
+                        required
+                    />
                 </div>
                 
                 <button type="submit" className="btn btn-primary">Submit</button>
